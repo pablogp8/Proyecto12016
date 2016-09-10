d16a94a0be4d1836413960cc632cd86540d0cfcc
Pablo Yeguech <pablo.ys@hotmail.com>
2016-09-10 06:38:37 -0600

    Carga de datos
    Carga de todos los archivos

---
+++ Lienzo2dComp2/src/lienzo2dcomp2/Lienzo2dComp2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lienzo2dcomp2;
+
+/**
+ *
+ * @author User
+ */
+public class Lienzo2dComp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventanaIni ventana=new ventanaIni();
+        ventana.show(true);
+    }
+    
+}
